7650571eef98f0710b18e1ebda16668f2ab2f862
GabrielGGC18 <gabrieldesamendes@gmai.com>
2026-05-21 17:27:55 -0300

    codigo

--- atividade1/src/atividade1/Animal.java
+++ atividade1/src/atividade1/Animal.java
@@ -1,18 +1,46 @@
 package atividade1;
 
-// TODO: renomear classe para "C_????" conforme o modelo
 public class Animal {
     private int id;
     private String nome;
     private Fazenda fazenda;
 
-    public Animal(int id; String nome; Fazenda fazenda;){
+    public Animal(int id, String nome, Fazenda fazenda) {
         this.id = id;
         this.nome = nome;
         this.fazenda = fazenda;
     }
 
-    // TODO: getters / setters
+    public int getId() {
+        return id;
+    }
 
-    // TODO: toString com todas as informacoes
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Fazenda getFazenda() {
+        return fazenda;
+    }
+
+    public void setFazenda(Fazenda fazenda) {
+        this.fazenda = fazenda;
+    }
+
+    @Override
+    public String toString() {
+        return "Animal{" +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
+                ", fazenda=" + fazenda +
+                '}';
+    }
 }
--- atividade1/src/atividade1/Main.java
+++ atividade1/src/atividade1/Main.java
@@ -3,6 +3,20 @@ package atividade1;
 public class Main {
     public static void main(String[] args) {
         // TODO: criar vetor da classe C_????
+        Animal[] animais = new Animal[6];
+        animais[0] = new Animal(0, "Joadanete", Fazenda.VACA);
+        animais[1] = new Animal(1, "Claudenilson", Fazenda.TOURO);
+        animais[2] = new Animal(2, "Lamborghini", Fazenda.CAVALO);
+        animais[3] = new Animal(3, "Mustang", Fazenda.PORCO);
+        animais[4] = new Animal(4, "Rooster", Fazenda.GALINHA);
+        animais[5] = new Animal(5, "Peixonauta", Fazenda.PEIXE);
+        for (int i = 0; i< animais.length; i++){
+            System.out.println(animais[i]);
+        }
+
+
+
+
         // TODO: instanciar e inserir objetos no vetor
         // TODO: percorrer e imprimir todas as informacoes
     }
